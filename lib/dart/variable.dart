589394e0c3c3bfe6432ecc6f152e6d62b42fc5fa
Pinchai <>
2024-09-09 18:53:53 +0700

    first date

---
+++ lib/dart/variable.dart
@@ -0,0 +1,26 @@
+void main(){
+  /*
+  * c++ int a = 10;
+  * c# int a = 10;
+  * php $a = 10;
+  * javascript var a = 10;
+  * java int a = 10;
+  * python a = 10;
+  * dart
+  *
+  *
+  * */
+  // String name = "dara";
+  // int age = 18;
+  // double salary = 100.5;
+  // //print(name+' - '+age.toString()+' - '+ salary.toString());
+  // print("Name: ${name} Age: ${age} Salary: ${salary}");
+
+
+  var firstName = 'Nicola';
+  var lastName = "Tesla";
+
+//can embed variables in string with $
+  String fullName = "${firstName.toUpperCase()} ${lastName.toUpperCase()}";
+  print(fullName);
+}
